0973986676e2cbded1b23a0479c498c5bb9ca09f
46042 - Rosario Machado <A46042@alunos.isel.pt>
2024-05-30 16:49:23 +0100

    1c working

--- app/src/main/java/isel/App.java
+++ app/src/main/java/isel/App.java
@@ -40,10 +40,8 @@ interface DbWorker
 {
     void doWork();
 }
-class UI
-{
-    private enum Option
-    {
+class UI {
+    private enum Option {
         // DO NOT CHANGE ANYTHING!
         Unknown,
         Exit,
@@ -55,40 +53,52 @@ class UI
         cancelBooking,
         about
     }
-    private static UI __instance = null;
-  
-    private HashMap<Option,DbWorker> __dbMethods;
 
-    private UI()
-    {
+    private static UI __instance = null;
+
+    private HashMap<Option, DbWorker> __dbMethods;
+
+    private UI() {
         // DO NOT CHANGE ANYTHING!
-        __dbMethods = new HashMap<Option,DbWorker>();
+        __dbMethods = new HashMap<Option, DbWorker>();
         __dbMethods.put(Option.createCostumer, () -> UI.this.createCostumer());
-        __dbMethods.put(Option.listExistingBikes, () -> UI.this.listExistingBikes()); 
-        __dbMethods.put(Option.checkBikeAvailability, () -> UI.this.checkBikeAvailability());      
-        __dbMethods.put(Option.obtainBookings, new DbWorker() {public void doWork() {UI.this.obtainBookings();}});
-        __dbMethods.put(Option.makeBooking, new DbWorker() {public void doWork() {UI.this.makeBooking();}});
-        __dbMethods.put(Option.cancelBooking, new DbWorker() {public void doWork() {UI.this.cancelBooking();}});
-        __dbMethods.put(Option.about, new DbWorker() {public void doWork() {UI.this.about();}});
+        __dbMethods.put(Option.listExistingBikes, () -> UI.this.listExistingBikes());
+        __dbMethods.put(Option.checkBikeAvailability, () -> UI.this.checkBikeAvailability());
+        __dbMethods.put(Option.obtainBookings, new DbWorker() {
+            public void doWork() {
+                UI.this.obtainBookings();
+            }
+        });
+        __dbMethods.put(Option.makeBooking, new DbWorker() {
+            public void doWork() {
+                UI.this.makeBooking();
+            }
+        });
+        __dbMethods.put(Option.cancelBooking, new DbWorker() {
+            public void doWork() {
+                UI.this.cancelBooking();
+            }
+        });
+        __dbMethods.put(Option.about, new DbWorker() {
+            public void doWork() {
+                UI.this.about();
+            }
+        });
 
     }
 
-    public static UI getInstance()
-    {
+    public static UI getInstance() {
         // DO NOT CHANGE ANYTHING!
-        if(__instance == null)
-        {
+        if (__instance == null) {
             __instance = new UI();
         }
         return __instance;
     }
 
-    private Option DisplayMenu()
-    {
+    private Option DisplayMenu() {
         Option option = Option.Unknown;
         Scanner s = new Scanner(System.in);
-        try
-        {
+        try {
             // DO NOT CHANGE ANYTHING!
             System.out.println("Bicycle reservation");
             System.out.println();
@@ -101,145 +111,129 @@ class UI
             System.out.println("7. Cancel Booking");
             System.out.println("8. About");
             System.out.print(">");
-            
+
             int result = s.nextInt();
             option = Option.values()[result];
 
-        }
-        catch(RuntimeException ex)
-        {
+        } catch (RuntimeException ex) {
             //nothing to do.
-        }
-        finally
-        {
+        } finally {
 
         }
         return option;
 
     }
-    private static void clearConsole() throws Exception
-    {
+
+    private static void clearConsole() throws Exception {
         // DO NOT CHANGE ANYTHING!
         for (int y = 0; y < 25; y++) //console is 80 columns and 25 lines
             System.out.println("\n");
 
     }
 
-    public void Run() throws Exception
-    {
+    public void Run() throws Exception {
         // DO NOT CHANGE ANYTHING!
         Option userInput;
-        do
-        {
+        do {
             clearConsole();
             userInput = DisplayMenu();
             clearConsole();
-            try
-            {
+            try {
                 __dbMethods.get(userInput).doWork();
                 System.in.read();
-            }
-            catch(NullPointerException ex)
-            {
+            } catch (NullPointerException ex) {
                 //Nothing to do. The option was not a valid one. Read another.
             }
 
-        }while(userInput!=Option.Exit);
+        } while (userInput != Option.Exit);
     }
 
     /**
-    To implement from this point forward. Do not need to change the code above.
-    -------------------------------------------------------------------------------     
-        IMPORTANT:
-    --- DO NOT MOVE IN THE CODE ABOVE. JUST HAVE TO IMPLEMENT THE METHODS BELOW ---
-    --- Other Methods and properties can be added to support implementation -------
-    -------------------------------------------------------------------------------
-    
-    */
+     * To implement from this point forward. Do not need to change the code above.
+     * -------------------------------------------------------------------------------
+     * IMPORTANT:
+     * --- DO NOT MOVE IN THE CODE ABOVE. JUST HAVE TO IMPLEMENT THE METHODS BELOW ---
+     * --- Other Methods and properties can be added to support implementation -------
+     * -------------------------------------------------------------------------------
+     */
 
     private void createCostumer() {
-        try (JPAContext ctx = new JPAContext()){
+        try (JPAContext ctx = new JPAContext()) {
 
             Cliente c = getClientFromConsole();
 
             ctx.getClientesRepo().create(c);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: " + e.getCause().toString() + " - " + e.getMessage());
         }
         System.out.println("createCostumer()");
     }
 
-    private Cliente getClientFromConsole(){
+    private Cliente getClientFromConsole() {
 
-            Scanner scanner = new Scanner(System.in);
-            Cliente cliente = new Cliente();
+        Scanner scanner = new Scanner(System.in);
+        Cliente cliente = new Cliente();
 
-            System.out.println("Nome do cliente: ");
-            cliente.setNome(scanner.nextLine());
+        System.out.println("Nome do cliente: ");
+        cliente.setNome(scanner.nextLine());
 
-            System.out.println("Morada do cliente: ");
-            cliente.setMorada(scanner.nextLine());
+        System.out.println("Morada do cliente: ");
+        cliente.setMorada(scanner.nextLine());
 
-            System.out.println("Email do cliente: ");
-            cliente.setEmail(scanner.nextLine());
+        System.out.println("Email do cliente: ");
+        cliente.setEmail(scanner.nextLine());
 
-            System.out.println("Telefone do cliente: ");
-            cliente.setTelefone(scanner.nextLine());
+        System.out.println("Telefone do cliente: ");
+        cliente.setTelefone(scanner.nextLine());
 
-            System.out.println("CC/Passaporte do cliente: ");
-            cliente.setCcPassaporte(scanner.nextLine());
+        System.out.println("CC/Passaporte do cliente: ");
+        cliente.setCcPassaporte(scanner.nextLine());
 
-            System.out.println("Nacionalidade do cliente: ");
-            cliente.setNacionalidade(scanner.nextLine());
+        System.out.println("Nacionalidade do cliente: ");
+        cliente.setNacionalidade(scanner.nextLine());
 
-            cliente.setAtivo(true);
+        cliente.setAtivo(true);
 
-            return cliente;
+        return cliente;
 
     }
-  
-    private void listExistingBikes()
-    {
-        try (JPAContext ctx = new JPAContext()){
+
+    private void listExistingBikes() {
+        try (JPAContext ctx = new JPAContext()) {
             List<Bicicleta> bikes = ctx.getBiciclesRepo().getBicycles();
             for (Bicicleta b : bikes) {
                 System.out.println(b.toString());
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error: " + e.getCause().toString() + " - " + e.getMessage());
         }
 
     }
 
-    private void checkBikeAvailability()
-    {
-        try (JPAContext ctx = new JPAContext()){
+    private void checkBikeAvailability() {
+        try (JPAContext ctx = new JPAContext()) {
+            Scanner scanner = new Scanner(System.in);
 
-            try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter bike ID: ");
+            int id = scanner.nextInt();
 
-                System.out.println("Id da bicicleta a validar: ");
-                Integer id = scanner.nextInt();
+            scanner.nextLine();
 
-                System.out.println("Data de inicio da reserva (dd-mm-yyyy): ");
-                Timestamp dataInicio = Timestamp.valueOf(scanner.nextLine());
+            System.out.print("Enter start date (yyyy-mm-dd hh:mm:ss): ");
+            Timestamp dataInicio = Timestamp.valueOf(scanner.nextLine());
 
-                System.out.println("Data de fim da reserva (dd-mm-yyyy): ");
-                Timestamp dataFim = Timestamp.valueOf(scanner.nextLine());
+            System.out.print("Enter end date (yyyy-mm-dd hh:mm:ss): ");
+            Timestamp dataFim = Timestamp.valueOf(scanner.nextLine());
 
-                Boolean result = ctx.getBiciclesRepo().podeSerReservado(id, dataInicio, dataFim);
+            Boolean result =  ctx.getBiciclesRepo().podeSerReservado(id, dataInicio, dataFim);
 
-                System.out.println("Pode ser reservado: " + result);
-            }
-
-        }
-        catch (Exception e){
+            System.out.println("Pode ser reservado: " + result);
+        } catch (Exception e) {
             System.out.println("Error: " + e.getCause().toString() + " - " + e.getMessage());
         }
-
-
     }
+
+
 
     private void obtainBookings() {
         // TODO
